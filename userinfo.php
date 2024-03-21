89cd9154481506e3bf260393c16f5c2edfaddf2d
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2024-03-21 23:29:44 +0530

    first commit

---
+++ userinfo.php
@@ -0,0 +1,20 @@
+<?php 
+$con=mysqli_connect('localhost','root');
+if($con){
+    echo"connection successful";
+}else{
+    echo"connection error";
+}
+
+mysqli_select_db($con,'gallarydata');
+
+$user = $_POST['user'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];
+$comment =  $_POST['comment'];
+
+$query=" insert into userinfodata (user,email,mobile,comment)  
+values ('$user','$email','$mobile','$comment') ";
+mysqli_query($con,$query);
+
+?>
